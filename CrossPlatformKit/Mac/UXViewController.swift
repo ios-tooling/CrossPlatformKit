e74af2d7ee2bab46fede5d0108cd8fd7cf85ce5b
Ben Gottlieb <ben@standalone.com>
2018-02-17 14:50:34 -0600

    added UXViewController

---
+++ CrossPlatformKit/Mac/UXViewController.swift
@@ -0,0 +1,12 @@
+//
+//  UXViewController.swift
+//  CrossPlatformKit_Mac
+//
+//  Created by Ben Gottlieb on 12/27/17.
+//  Copyright © 2017 Stand Alone, inc. All rights reserved.
+//
+
+import Cocoa
+
+public typealias UXViewController = NSViewController
+
---
+++ CrossPlatformKit/iOS/UXViewController.swift
@@ -0,0 +1,11 @@
+//
+//  UXViewController.swift
+//  CrossPlatformKit_iOS
+//
+//  Created by Ben Gottlieb on 12/27/17.
+//  Copyright © 2017 Stand Alone, inc. All rights reserved.
+//
+
+import UIKit
+
+public typealias UXViewController = UIViewController
