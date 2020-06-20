94243baffde14adb5efb08f8c0af6ecd1ce683fb
Ben Gottlieb <ben@standalone.com>
2020-06-19 19:22:36 -0500

    added UXApplication

---
+++ Source/CrossPlatformKit/UXApplication+Mac.swift
@@ -0,0 +1,15 @@
+
+//
+//  UXApplication+Mac.swift
+//  
+//
+//  Created by Ben Gottlieb on 6/19/20.
+//
+
+#if os(OSX)
+import AppKit
+import Cocoa
+
+public typealias UXApplication = NSApplication
+
+#endif
---
+++ Source/CrossPlatformKit/UXApplication+iOS.swift
@@ -0,0 +1,14 @@
+//
+//  UXApplication+iOS.swift
+//  
+//
+//  Created by Ben Gottlieb on 6/19/20.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+public typealias UXApplication = UIApplication
+
+
+#endif
