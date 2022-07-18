560183de64539b24c675f67a0806a29303b84609
Ben Gottlieb <ben@standalone.com>
2022-07-18 18:42:13 -0500

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CrossPlatformKit",
+	 platforms: [
+				 .macOS(.v11),
+				 .iOS(.v13),
+				 .watchOS(.v7)
+		],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
 		.library(
