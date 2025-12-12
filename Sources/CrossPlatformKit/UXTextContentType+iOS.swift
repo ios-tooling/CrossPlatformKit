ae2a06de27c3a204e69845b6d58ac69316c759db
Ben Gottlieb <ben@standalone.com>
2025-12-11 22:45:19 -0600

    added UXTextContentType

---
+++ Sources/CrossPlatformKit/UXTextContentType+iOS.swift
@@ -0,0 +1,12 @@
+//
+//  UXTextContentType+iOS.swift
+//  CrossPlatformKit
+//
+//  Created by Ben Gottlieb on 12/11/25.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+public typealias UXTextContentType = UITextContentType
+#endif
---
+++ Sources/CrossPlatformKit/UXTextContentType+macOS.swift
@@ -0,0 +1,12 @@
+//
+//  UXTextContentType+macOS.swift
+//  CrossPlatformKit
+//
+//  Created by Ben Gottlieb on 12/11/25.
+//
+
+#if canImport(AppKit)
+import AppKit
+
+public typealias UXTextContentType = NSTextContentType
+#endif
